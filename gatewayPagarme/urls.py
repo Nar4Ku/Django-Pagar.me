bf1aaa67ba131600588754b85f58812f3682e617
Luis Fernando Petreca <lfpetreca02@gmail.com>
2019-04-23 12:00:20 -0300

    Get the postback URL

--- gatewayPagarme/urls.py
+++ gatewayPagarme/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from home.views import test, success
+from home.views import test, success, notification
 from accounts.views import login_view, register_view, logout_view
 
 urlpatterns = [
@@ -9,5 +9,6 @@ urlpatterns = [
     path('logout', logout_view, name="logout"),
     path('sign-up/', register_view, name="sign_in"),
     path('home/', test, name="test"),
-    path('success/', success, name="success")
+    path('success/', success, name="success"),
+    path('post-back/', notification, name="notification")
 ]
--- home/views.py
+++ home/views.py
@@ -1,10 +1,11 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 import pagarme
+from prettyprinter import pprint
 
 #You can pass your api_keys as enviroment variable if you wanna
-CRYP_KEY = 'Your Cryp Pass'
+CRYP_KEY = 'CRYP PASS'
 
-@login_required 
 def test(request):
     # You cryp key, founded in Pagar.me dashboard
     key = CRYP_KEY
@@ -33,11 +34,17 @@ def test(request):
     # Set the max number of installments you wish. But remember, pagar.me only accept values lower or equal to 12.
     maxInstallments = 3
 
+    # getUserId or IdTransation for identification of transation.
+    metadata = {
+        'external_id': '6668666'
+    }
+
     context = { 
         'key': key,
         'valor_compra': amount,
         'lista_items': items,
-        'installments': maxInstallments
+        'installments': maxInstallments,
+        'metadata': metadata
         }
 
     return render(request, 'test.html', context)
@@ -45,3 +52,19 @@ def test(request):
 def success(request):
     context = {}
     return render(request, 'success.html', context)
+
+@csrf_exempt
+def notification(request):
+    # Create here your notification scripts of post-back
+    if request.method == 'POST':
+
+        res = request.POST
+        pprint(res)    #remove after debuging
+        if res['event'] == 'transaction_status_changed':
+            
+            print(res['transaction[customer][external_id]']) #email type by user in modal
+            print(res['current_status'])
+
+            # Alter the value of the status etc.
+
+    return render(request, 'notification.html', {})
